c8bb83a85e57706ab00ddb2f75419d0ec672e8d3
Ellis Min <dbmin@hotmail.com>
2020-01-24 08:08:20 -0600

    first commit

---
+++ static/precache-manifest.2b9bb3ebac4477d2f795cd7d332fa572.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8bba836e8088da2bec6f31a4b98fe1",
+    "url": "/restaurant-app-public/index.html"
+  },
+  {
+    "revision": "c96be2c96b5a7204ab9b",
+    "url": "/restaurant-app-public/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8152bfcec73c2e4fef43",
+    "url": "/restaurant-app-public/static/css/main.79e1cced.chunk.css"
+  },
+  {
+    "revision": "c96be2c96b5a7204ab9b",
+    "url": "/restaurant-app-public/static/js/2.02abbdff.chunk.js"
+  },
+  {
+    "revision": "c9d490cefc26f07be1cfc2b04269560c",
+    "url": "/restaurant-app-public/static/js/2.02abbdff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8152bfcec73c2e4fef43",
+    "url": "/restaurant-app-public/static/js/main.a5efcb3d.chunk.js"
+  },
+  {
+    "revision": "7e1b53ee012c211ce887",
+    "url": "/restaurant-app-public/static/js/runtime-main.c6e7b481.js"
+  },
+  {
+    "revision": "a58f1b8a8849113d557711cca3e31a3f",
+    "url": "/restaurant-app-public/static/media/bathroom.a58f1b8a.png"
+  },
+  {
+    "revision": "40b9e1fcd89eb3944c377bcce238780e",
+    "url": "/restaurant-app-public/static/media/delivery.40b9e1fc.jpeg"
+  },
+  {
+    "revision": "f4364a8100be4ff4f6b4db7829926b4b",
+    "url": "/restaurant-app-public/static/media/logo-square.f4364a81.png"
+  },
+  {
+    "revision": "34047ef1f95eced0623ba64662c80802",
+    "url": "/restaurant-app-public/static/media/menu-2.34047ef1.jpeg"
+  },
+  {
+    "revision": "1cf1135f16c43779f244cf7260e6f16d",
+    "url": "/restaurant-app-public/static/media/no-image.1cf1135f.png"
+  },
+  {
+    "revision": "e0f23a3ad736bbb7b7403b3a8fcd571c",
+    "url": "/restaurant-app-public/static/media/noodle-color.e0f23a3a.png"
+  },
+  {
+    "revision": "73681ceaf564195b089b25ed49bb5cdc",
+    "url": "/restaurant-app-public/static/media/ramen.73681cea.png"
+  },
+  {
+    "revision": "7af57b318924107095569657e8ed3968",
+    "url": "/restaurant-app-public/static/media/slide-1.7af57b31.png"
+  },
+  {
+    "revision": "06d7dc5c61ca8bb9aba6400ed39b267b",
+    "url": "/restaurant-app-public/static/media/slide-2.06d7dc5c.png"
+  }
+]);
